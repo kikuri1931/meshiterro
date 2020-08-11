1d09376af14a8940a9728a1476aa153797ab642d
kikuri1931 <ys1945050mite.blue@gmail.com>
2020-08-11 16:10:34 +0900

    [Finsish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6d524c09627e8c0fd5d91c1b8f07080b63566e32759ff47bf9da24a17e6bc55152c818d7013cd589943a0e55de4fde69f089ba5428b08418b4958994d6069b1d'
